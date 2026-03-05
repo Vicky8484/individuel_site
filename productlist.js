0cbb29d7d46dcf9624a5d361ce99fce9fe04744e
Vicky8484 <viok0001@stud.ek.dk>
2026-03-05 20:34:06 +0100

    completely done dynamisk site

--- productlist.js
+++ productlist.js
@@ -33,4 +33,89 @@ function showProducts(products) {
   });
 }
 
+// Pris Sortering
+const sortByPriceBtn = document.querySelector("#sortByPriceBtn");
+const sortByPriceBtn2 = document.querySelector("#sortByPriceBtn2");
+
+let allProducts = [];
+
+function getProducts() {
+  fetch(listURL)
+    .then((res) => res.json())
+    .then((products) => {
+      allProducts = products; // gem originaldata
+      showProducts(allProducts);
+    });
+}
+
+function sortPrice() {
+  console.log("sort by price");
+  const sorted = [...allProducts].sort((a, b) => a.price - b.price); // sortér efter pris
+  showProducts(sorted); // vis sorteret liste
+}
+
+sortByPriceBtn.addEventListener("click", sortPrice);
+
+// Køn Sortering
+const filterWomenBtn = document.querySelector("#sortByWomen");
+const showAllBtn = document.querySelector("#sortByAll");
+
+function filterByGender(targetGender) {
+  const filtered = allProducts.filter((product) => (product.gender || "").toLowerCase() === targetGender.toLowerCase());
+
+  showProducts(filtered);
+}
+
+filterWomenBtn.addEventListener("click", () => filterByGender("Women"));
+showAllBtn.addEventListener("click", () => showProducts(allProducts));
+
+// Article type Sortering (som vi ikke bruger alligevel, fordi det kun er relevant for Accessories product list)
+// Filter Bags
+/*const filterBagsBtn = document.querySelector("#sortByBags");
+
+function filterByBags(targetBags) {
+  const filtered = allProducts.filter((product) => (product.articletype || "").toLowerCase() === targetBags.toLowerCase());
+
+  showProducts(filtered);
+}
+
+filterBagsBtn.addEventListener("click", () => filterByBags("Backpacks"));
+showAllBtn.addEventListener("click", () => showProducts(allProducts));
+
+// Filter Hats
+const filterHatsBtn = document.querySelector("#sortByHats");
+
+function filterByHats(targetHats) {
+  const filtered = allProducts.filter((product) => (product.articletype || "").toLowerCase() === targetHats.toLowerCase());
+
+  showProducts(filtered);
+}
+
+filterHatsBtn.addEventListener("click", () => filterByHats("Water Bottle"));
+showAllBtn.addEventListener("click", () => showProducts(allProducts));*/
+
+// Discount Sortering
+const filterDiscountBtn = document.querySelector("#sortByDiscount");
+
+function filterByDiscount() {
+  const filtered = allProducts.filter((product) => product.discount > 0);
+
+  showProducts(filtered);
+}
+
+filterDiscountBtn.addEventListener("click", filterByDiscount);
+showAllBtn.addEventListener("click", () => showProducts(allProducts));
+
+//På lager Sortering
+const filterStockBtn = document.querySelector("#sortByStock");
+
+function filterByStock() {
+  const filtered = allProducts.filter((product) => product.soldout === 0);
+
+  showProducts(filtered);
+}
+
+filterStockBtn.addEventListener("click", filterByStock);
+showAllBtn.addEventListener("click", () => showProducts(allProducts));
+
 getProducts();
